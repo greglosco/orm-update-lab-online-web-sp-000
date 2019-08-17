2a55c3bb354265f32b076e5bd99335dd0bcca3c5
Gregory Losco <gtlosco@gmail.com>
2019-08-17 23:26:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,7 +24,21 @@ class Student
   end
   
   def self.drop_table
+    sql = "DROP TABLE students"
     
-
-
+    DB[:conn].execute(sql)
+  end
+  
+  def save 
+    sql = <<-SQL
+      INSERT INTO students (name, grade_)
+    SQL
+  end
+  
+  
+  
+  
+  
+  
+  
 end
